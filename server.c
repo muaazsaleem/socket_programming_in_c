498d0abf6de957db2cc40aa8ad4923f45fe2c0b4
Muaaz Saleem <muaazsaleem@outlook.com>
2014-09-08 10:20:53 +0500

    Tutorial Complete

--- server.c
+++ server.c
@@ -1,55 +1,57 @@
 /* A simple server in the internet domain using TCP
    The port number is passed as an argument */
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> //for standard i/o
+#include <stdlib.h> 
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include <sys/types.h> // for data types definitions used in system calls
+#include <sys/socket.h> //definition of structures needed for sockets
+#include <netinet/in.h> // constants and structures for internet domain
 
 void error(const char *msg)
 {
-    perror(msg);
+    perror(msg); //called when a sys call fails. Displays the error on stderror and exits the program
     exit(1);
 }
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno;
-     socklen_t clilen;
+     int socket_file_descriptor, new_socket_file_descriptor, portno, n; //n is the chars read or written
+     socklen_t client_address_length;
      char buffer[256];
      struct sockaddr_in serv_addr, cli_addr;
-     int n;
+     
      if (argc < 2) {
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
+     socket_file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+     if (socket_file_descriptor < 0) 
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
      portno = atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
+     serv_addr.sin_port = htons(portno); // converts portno to Network byte order
+     if (bind(socket_file_descriptor, (struct sockaddr *) &serv_addr, sizeof(serv_addr))
+             < 0) 
               error("ERROR on binding");
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, 
+     listen(socket_file_descriptor,5);
+     client_address_length = sizeof(cli_addr);
+     new_socket_file_descriptor = accept(socket_file_descriptor, 
                  (struct sockaddr *) &cli_addr, 
-                 &clilen);
-     if (newsockfd < 0) 
+                 &client_address_length);
+     if (new_socket_file_descriptor < 0) 
           error("ERROR on accept");
      bzero(buffer,256);
-     n = read(newsockfd,buffer,255);
+     n = read(new_socket_file_descriptor,buffer,255); //(fro,to,length)
      if (n < 0) error("ERROR reading from socket");
      printf("Here is the message: %s\n",buffer);
-     n = write(newsockfd,"I got your message",18);
+     n = write(new_socket_file_descriptor,"I got your message",18);
      if (n < 0) error("ERROR writing to socket");
-     close(newsockfd);
-     close(sockfd);
+     
+     close(new_socket_file_descriptor);
+     close(socket_file_descriptor);
+     
      return 0; 
 }
